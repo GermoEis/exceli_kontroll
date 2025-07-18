cb60eea111519a8dc9f4f356e59e61996929208b
GermoEis <germoeismann1@gmail.com>
2025-07-18 13:13:13 +0300

    parandus

--- build_all.bat
+++ build_all.bat
@@ -2,9 +2,9 @@
 echo Alustan EXE-de loomist...
 
 python -m PyInstaller Exceli_kontroll.spec
-python -m PyInstaller excelite_võrdlus.spec
+python -m PyInstaller excelite_vordlus.spec
 python -m PyInstaller Kontroll.spec
-python -m PyInstaller kuupäeva_kontroll.spec
+python -m PyInstaller kuupaeva_kontroll.spec
 python -m PyInstaller xml_muutmine.spec
 
 echo Valmis! Exe failid on kaustas dist\
